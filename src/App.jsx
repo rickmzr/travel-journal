4a1618dea71ac7e6211ec184c38534fd0bb19083
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-06-19 13:42:20 -0500

    Finish assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header";
 import TravelCard from "./components/TravelCard";
+import data from "./assets/data";
 import "./App.css";
 
 function App() {
+  const cards = data.map((data, i) => {
+    return <TravelCard key={i} item={data} />;
+  });
+
   return (
     <>
       <Header />
+      <section className="cards-section">{cards}</section>
     </>
   );
 }
--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,7 +1,7 @@
-export default function TravelCard() {
+export default function TravelCard(props) {
   return (
     <div className="card-container">
-      <img src="" alt="" className="card-pic" />
+      <img src={props.item.imageUrl} alt="" className="card-pic" />
       <div className="content-container">
         <div className="content-header">
           <img
@@ -9,12 +9,12 @@ export default function TravelCard() {
             alt="Map Pin Icon"
             className="maps-icon"
           />
-          <span className="country"></span>
-          <a href="">View on Google Maps</a>
+          <span className="country">{props.item.location}</span>
+          <a href={props.item.googleMapsUrl}>View on Google Maps</a>
         </div>
-        <h1 className="title"></h1>
-        <h4 className="date"></h4>
-        <p className="description"></p>
+        <h1 className="title">{props.item.title}</h1>
+        <h4 className="date">{`${props.item.startDate} - ${props.item.endDate}`}</h4>
+        <p className="description">{props.item.description}</p>
       </div>
     </div>
   );
